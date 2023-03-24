276e9c51639476b30801a1e0b68fd2d394fa3906
Alina Mavlikaeva <mavl.alina@yandex.com>
2023-03-24 21:16:31 +0200

    feauture:added java script and popup

---
+++ script/index.js
@@ -0,0 +1,33 @@
+const editButton = document.querySelector('.profile__edit-button_opened');
+const closeButton = document.querySelector('.popup__close-button_closed');
+const submitButton = document.querySelector('.popup__submit_saved');
+let formElement = document.querySelector(".popup");
+let nameValue = document.querySelector(".profile__name");  
+let jobValue = document.querySelector(".profile__description"); 
+let nameInput = document.querySelector('.popup__name');
+let jobInput = document.querySelector('.popup__description');
+
+
+function openedPopup(){
+  formElement.classList.add('popup_opened');
+  nameInput.value = nameValue.textContent;
+  jobInput.value = jobValue.textContent;
+
+}
+
+function closedPopup(){
+    formElement.classList.remove('popup_opened');
+}
+
+editButton.addEventListener('click', openedPopup);
+closeButton.addEventListener('click', closedPopup);
+
+
+function handleFormSubmit (evt) {
+    evt.preventDefault(); 
+    nameValue.textContent = nameInput.value;
+    jobValue.textContent = jobInput.value;
+    console.log(jobValue.textContent);
+}
+
+formElement.addEventListener(formElement, handleFormSubmit); 
