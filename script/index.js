21d4f415fb987370e49e44ae9484d70564292e4a
Alina Mavlikaeva <mavl.alina@yandex.com>
2023-03-29 01:13:56 +0300

    hotfix:syntax,style,js-functions errors

--- script/index.js
+++ script/index.js
@@ -1,22 +1,21 @@
-const editButton = document.querySelector('.profile__edit-button_opened');
-const closeButton = document.querySelector('.popup__close-button_closed');
-const submitButton = document.querySelector('.popup__submit_saved');
-let formElement = document.querySelector(".popup");
+const editButton = document.querySelector('.profile__edit-button');
+const closeButton = document.querySelector('.popup__close-button');
+let popupElement = document.querySelector(".popup");
 let nameValue = document.querySelector(".profile__name");  
 let jobValue = document.querySelector(".profile__description"); 
-let nameInput = document.querySelector('.popup__name');
-let jobInput = document.querySelector('.popup__description');
+let nameInput = document.querySelector('.popup__input_name_typed');
+let jobInput = document.querySelector('.popup__input_description_typed');
+const formElement = document.querySelector('.popup__form');
 
 
 function openedPopup(){
-  formElement.classList.add('popup_opened');
+  popupElement.classList.add('popup_opened');
   nameInput.value = nameValue.textContent;
   jobInput.value = jobValue.textContent;
-
 }
 
 function closedPopup(){
-    formElement.classList.remove('popup_opened');
+  popupElement.classList.remove('popup_opened');
 }
 
 editButton.addEventListener('click', openedPopup);
@@ -26,7 +25,8 @@ closeButton.addEventListener('click', closedPopup);
 function handleFormSubmit (evt) {
     evt.preventDefault(); 
     nameValue.textContent = nameInput.value;
-    jobValue.textContent = jobInput.value;
+    jobValue.textContent = jobInput.value;  
+    popupElement.classList.remove('popup_opened');  
 }
 
-formElement.addEventListener(formElement, handleFormSubmit); 
+formElement.addEventListener('submit',handleFormSubmit); 
